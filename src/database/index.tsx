97cde3121f869d986e0981872770008c08614f76
Jean Dresbach <jeanr.dresbach@gmail.com>
2024-01-16 18:26:23 -0300

    database atualizada

--- src/database/index.tsx
+++ src/database/index.tsx
@@ -1,8 +1,45 @@
+import jeanImg from "../assets/foto-perfil-jean.png"
+
 interface Person {
   name: string
-  softSkills: string[]
+  surname: string
+  qualities: string[]
   color: string
   linkdin: string
+  image: string
 }
 
-export const people: Person[] = []
+export const people: Person[] = [
+  {
+    name: "Jean",
+    surname: "Dresbach",
+    color: "#30b5f0",
+    qualities: ["Colaborativo", "Raciocinio lógico", "Inteligente"],
+    linkdin: "www.linkedin.com/in/jean-dresbach-a98a90293",
+    image: jeanImg,
+  },
+  {
+    name: "Eduardo",
+    surname: "Faria",
+    color: "#1cc013",
+    qualities: ["Colaborativo", "Esforcado", "Relacionamento interpessoal"],
+    linkdin: "https://www.linkedin.com/in/eduardo-vaz-de-faria-5573bb196/",
+    image: "",
+  },
+  {
+    name: "Mateus",
+    surname: "Luz",
+    color: "#d41548",
+    qualities: ["Colaborativo", "Prestativo", "Comunicativo"],
+    linkdin: "#",
+    image: "",
+  },
+  {
+    name: "Felipe",
+    surname: "Sinn",
+    color: "#ffa300",
+    qualities: ["", "", ""],
+    linkdin: "#",
+    image: "",
+  },
+]
